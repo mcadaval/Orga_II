562f8a8702d59275f5a3a61fc6f21bee4a298b1c
Hildorien <nachofernandez.1995@hotmail.com>
2016-09-08 20:11:33 -0300

    Esta hecho rotar en c, estoy laburando con combinar en c

--- TP2/codigo/filtros/combinar_c.c
+++ TP2/codigo/filtros/combinar_c.c
@@ -16,8 +16,27 @@ void combinar_c (
 	int dst_row_size,
 	float alpha
 ) {
+
 	unsigned char (*src_matrix)[src_row_size] = (unsigned char (*)[src_row_size]) src;
 	unsigned char (*dst_matrix)[dst_row_size] = (unsigned char (*)[dst_row_size]) dst;
 
-	//COMPLETAR
+	
+	for (int i = 0; i<filas; i+=1) {          	//voltear horizontal la imagen dst (sacado de utils.c)
+		for (int j = 0; j<cols; j+=1) {
+			dst_matrix[i][cols-j-1] = src_matrix[i][j];
+		}
+	}
+	
+	for (int f = 0; f < filas; f++) {   		//recorro la imagen dst aplicando la formula con el dst viejo(el volteado) y src  
+		for (int c = 0; c < cols; c++) {   
+			bgra_t *p_d = (bgra_t*) &dst_matrix[f][c * 4];
+            bgra_t *p_s = (bgra_t*) &src_matrix[f][c * 4];
+            
+			p_d->b = ( (alpha * (p_s->b - p_d->b)) / (float)255.00) + p_d->b;      
+			p_d->g = ( (alpha * (p_s->g - p_d->g)) / (float)255.00) + p_d->g;
+			p_d->r = ( (alpha * (p_s->r - p_d->r)) / (float)255.00) + p_d->r;
+			p_d->a = p_s->a;
+
+		}
+	}
 }
--- TP2/codigo/filtros/rotar_canales_c.c
+++ TP2/codigo/filtros/rotar_canales_c.c
@@ -5,6 +5,17 @@ void rotar_c (unsigned char *src, unsigned char *dst, int cols, int filas, int s
 	unsigned char (*src_matrix)[src_row_size] = (unsigned char (*)[src_row_size]) src;
 	unsigned char (*dst_matrix)[dst_row_size] = (unsigned char (*)[dst_row_size]) dst;
 
-	//COMPLETAR
+	for (int f = 0; f < filas; f++) {
+		for (int c = 0; c < cols; c++) {
+			bgra_t *p_d = (bgra_t*) &dst_matrix[f][c * 4];
+            bgra_t *p_s = (bgra_t*) &src_matrix[f][c * 4];
+
+			p_d->b = p_s->g;
+			p_d->g = p_s->r;
+			p_d->r = p_s->b;
+			p_d->a = p_s->a;
+
+		}
+	}
 }
 
